98d60762eec40418a2268f6e5cd237366a2bce37
Minnowo <62451415+Minnowo@users.noreply.github.com>
2023-09-10 18:10:08 -0400

    Removed debug code

--- dscrape/__init__.py
+++ dscrape/__init__.py
@@ -19,7 +19,7 @@ def scrape_course_information(dumper: extractor.CourseScraper, debug_break_1=Fal
     try:
         dumper = dumper()
 
-        dumper.scrape_and_dump(debug_break_1 or True)
+        dumper.scrape_and_dump(debug_break_1)
         
         logging.debug(f"Course dumper id is {dumper.school_id}")
         
